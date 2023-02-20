7d1a79b30730cc8c0c26897b907fc0a770068993
GeorgeManikhas <115165734+GeorgeManikhas@users.noreply.github.com>
2023-02-20 16:20:19 +0300

    Add files via upload

---
+++ Swork/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] standartarray = new string[13] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan", "098", "USB"};
+string[] modarray = new string[standartarray.Length];
+
+void Modifiedarray(string[] standartarray, string[] modarray)
+{
+    int count = 0;
+    for (int i = 0; i < standartarray.Length; i++)
+    {
+    if(standartarray[i].Length <= 3)
+        {
+        modarray[count] = standartarray[i];
+        count++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Modifiedarray(standartarray, modarray);
+ShowArray(modarray);
+
